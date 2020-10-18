4e525c3f61c4eb605c0e340d1ac5b6b7132f8f72
Ignacio Marín Brañas <ignaciomarinbranas@MacBook-Pro.local>
2020-10-18 03:03:08 +0200

    added quick look for correlation between alcohol and speeding

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import seaborn as sns
+import matplotlib.pyplot as plt
 
 df = sns.load_dataset('car_crashes')
 
-print(df.head())
+print(df.head().to_string())
+df.plot(x='alcohol', y='speeding', kind='scatter')
+plt.show()
+
+
+print(df[['alcohol', 'speeding']].corr())

788e97739c116d78071f4630a6ee13b34f4cd64f
agarciamorales23455 <agarciamorales23455@gmail.com>
2020-10-18 14:01:04 +0200

    added comments

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import sys
 
+# making some changes to play around
 df = sns.load_dataset('car_crashes')
 
+# some more changes
 print(df.head().to_string())
 df.plot(x='alcohol', y='speeding', kind='scatter')
 plt.show()
 
-
+# and a little more
 print(df[['alcohol', 'speeding']].corr())
 sys.exit(0)
